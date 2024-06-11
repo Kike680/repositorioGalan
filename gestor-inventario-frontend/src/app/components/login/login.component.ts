a3b4549e343ec489a428765e3d06071c75b0638b
Enrique <egalanmoreno@sescam.jccm.es>
2024-06-11 12:26:24 +0200

    Login con aspecto consistente y ya obteniendo token

--- gestor-inventario-frontend/src/app/components/login/login.component.ts
+++ gestor-inventario-frontend/src/app/components/login/login.component.ts
@@ -1,12 +1,47 @@
 import { Component } from '@angular/core';
+import {AuthService} from "../../services/auth.service";
+import {Router, RouterLink} from "@angular/router";
+import Swal from 'sweetalert2';
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule, RouterLink
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+  username:string= '';
+  password:string= '';
+  error:string= '';
+
+  constructor(private authService: AuthService, private router:Router) {}
+
+  onSubmit():void{
+    this.authService.login(this.username,this.password).subscribe({
+      next:()=>{
+        Swal.fire({
+          title: "Usuario autenticado",
+          text: "Bienvenido" +this.username,
+          icon: "success"
+        });
+        this.router.navigate(['/home']);
+        console.log("Token de acceso :  " + this.authService.getToken());
+      },
+      error : ()=>{
+        Swal.fire({
+          title: "Credenciales inválidas",
+          text: "Verifique sus credenciales",
+          icon: "error"
+        });
+        this.error= 'Credenciales invalidas';
+      }
+    })
+  }
+
 
 }
